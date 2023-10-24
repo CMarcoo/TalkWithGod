0916ef2755aed4b5f4c462a0fc3b1c3a39030737
TheViperShow <thevipershowita@gmail.com>
2023-10-24 21:46:27 +0200

    Initial commit

---
+++ src/main/java/top/cmarco/talkwithgod/GodValues.java
@@ -0,0 +1,2 @@
+package top.cmarco.talkwithgod;public class GodValues {
+}
---
+++ src/main/java/top/cmarco/talkwithgod/TalkWithGod.java
@@ -0,0 +1,17 @@
+package top.cmarco.talkwithgod;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TalkWithGod extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
